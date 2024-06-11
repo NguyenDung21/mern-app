d777f8f0f5fdd13343743859c51314ae7d80dbbf
mamcoi12@gmail.com <mamcoi12@gmail.com>
2024-06-11 22:01:27 +0700

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connect to Mongodb successfully");
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000, ()=>{
     console.log("Server listenning port 3000!!");
